b6e38a87001226f50afaa2f50cf740dfc3ca8196
Gulser Kayacan <gkayacan2014@gmail.com>
2022-12-26 00:23:37 +0300

    26.12.2022

---
+++ src/test/java/Soru_01.java
@@ -0,0 +1,77 @@
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.checkerframework.checker.units.qual.K;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+public class Soru_01 {
+    static WebDriver driver;
+    @BeforeClass
+    public static void beforeClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("http://automationexercise.com");
+    }
+    @Test
+    public void test01() {
+        WebElement soru = driver.findElement(By.xpath("//*[@lang='en']"));
+        Assert.assertTrue(soru.isDisplayed());
+    }
+    @Test
+    public void test02() {
+        driver.findElement(By.xpath("//*[@class='fa fa-lock']")).click();
+    }
+    @Test
+    public void test03() {
+        WebElement sign = driver.findElement(By.xpath("//*[text()='New User Signup!']"));
+        Assert.assertTrue(sign.isDisplayed());
+    }
+    @Test
+    public void test04() {
+        driver.findElement(By.xpath("//*[@name='name']")).sendKeys("derrmaan", Keys.TAB, "derrman@hootm.com", Keys.ENTER);
+    }
+    @Test
+    public void test05(){
+        WebElement account= driver.findElement(By.xpath("//*[text()='Enter Account Information']"));
+        Assert.assertTrue(account.isDisplayed());
+    }
+    @Test
+    public void test06(){
+        driver.findElement(By.xpath("//*[@id='id_gender1']"));
+        driver.findElement(By.xpath("//*[@id='name']"))
+                .sendKeys("",Keys.TAB,
+                        "1234",Keys.TAB,
+                        "12",Keys.TAB,
+                        "August",Keys.TAB,
+                        "1999",Keys.TAB);
+        driver.findElement(By.xpath("(//*[@type='checkbox'])[1]")).click();
+        driver.findElement(By.xpath("(//*[@type='checkbox'])[2]")).click();
+        driver.findElement(By.xpath("//*[@id='first_name']"))
+                .sendKeys("mustafa",Keys.TAB,
+                        "derman",Keys.TAB,
+                        "derman inc",Keys.TAB,
+                        "bursa",Keys.TAB,
+                        "bursa",Keys.TAB,
+                        "Isr",Keys.TAB,
+                        "dsfv", Keys.TAB,
+                        "hdgfd",Keys.TAB,
+                        "hgdfbvd",Keys.TAB,
+                        "gfbv",Keys.TAB,Keys.ENTER);
+
+        //import org.openqa.selenium.Keys;
+        //WebDriver driver = # Selenium webdriver nesnesi oluşturun
+        //for (int i = 0; i < 10; i++) {
+        //    driver.findElement(By.tagName("body")).sendKeys(Keys.TAB);
+        //}
+
+
+    }
+}
